b3d89ca1b24c20519aae10025d1786af89e78bb3
Trond Eirik Haavarstein <haavarstein@gmail.com>
2020-09-29 02:17:25 -0700

    Add files via upload

--- Examples/VMware/Auto-PoC.ps1
+++ Examples/VMware/Auto-PoC.ps1
@@ -1,17 +1,57 @@
-﻿# PowerShell Wrapper for MDT, Standalone and Chocolatey Installation - (C)2015 xenappblog.com 
+﻿Function Set-SecureBoot {
+    <#
+    .SYNOPSIS Enable/Disable Seure Boot setting for a VM in vSphere 6.5
+    .NOTES  Author:  William Lam
+    .NOTES  Site:    www.virtuallyghetto.com
+    .PARAMETER Vm
+      VM to enable/disable Secure Boot
+    .EXAMPLE
+      Get-VM -Name Windows10 | Set-SecureBoot -Enabled
+    .EXAMPLE
+      Get-VM -Name Windows10 | Set-SecureBoot -Disabled
+    #>
+    param(
+        [Parameter(
+            Mandatory=$true,
+            ValueFromPipeline=$true,
+            ValueFromPipelineByPropertyName=$true)
+        ]
+        [VMware.VimAutomation.ViCore.Impl.V1.Inventory.InventoryItemImpl]$Vm,
+        [Switch]$Enabled,
+        [Switch]$Disabled
+     )
 
+    if($Enabled) {
+        $secureBootSetting = $true
+        $reconfigMessage = "Enabling Secure Boot for $Vm"
+    }
+    if($Disabled) {
+        $secureBootSetting = $false
+        $reconfigMessage = "Disabling Secure Boot for $Vm"
+    }
+
+    $spec = New-Object VMware.Vim.VirtualMachineConfigSpec
+    $bootOptions = New-Object VMware.Vim.VirtualMachineBootOptions
+    $bootOptions.EfiSecureBootEnabled = $secureBootSetting
+    $spec.BootOptions = $bootOptions
+  
+    #Write-Host "`n$reconfigMessage ..."
+    $task = $vm.ExtensionData.ReconfigVM_Task($spec)
+    $task1 = Get-Task -Id ("Task-$($task.value)")
+    $task1 | Wait-Task | Out-Null
+}
+
+# PowerShell Wrapper for MDT, Standalone and Chocolatey Installation - (C)2015 xenappblog.com 
 # Example 1: Start-Process "XenDesktopServerSetup.exe" -ArgumentList $unattendedArgs -Wait -Passthru
-
 # Example 2 Powershell: Start-Process powershell.exe -ExecutionPolicy bypass -file $Destination
-
 # Example 3 EXE (Always use ' '):
 # $UnattendedArgs='/qn'
 # (Start-Process "$PackageName.$InstallerType" $UnattendedArgs -Wait -Passthru).ExitCode
-
 # Example 4 MSI (Always use " "):
 # $UnattendedArgs = "/i $PackageName.$InstallerType ALLUSERS=1 /qn /liewa $LogApp"
 # (Start-Process msiexec.exe -ArgumentList $UnattendedArgs -Wait -Passthru).ExitCode
 
+Clear-Host
 Write-Verbose "Setting Arguments" -Verbose
 $StartDTM = (Get-Date)	
 
@@ -31,16 +71,22 @@ $VMGuestOS = $MyConfigFile.Post.VMGuestOS
 $ISO = $MyConfigFile.Post.ISO
 $ESXi = $MyConfigFile.Post.ESXi		# ESXi Standalone Host when not using $VMCluster
 
+if (!(Test-Path -Path "C:\Program Files\PackageManagement\ProviderAssemblies\nuget")) {Find-PackageProvider -Name 'Nuget' -ForceBootstrap -IncludeDependencies}
+Set-PSRepository -Name "PSGallery" -InstallationPolicy Trusted
+if (!(Get-Module -ListAvailable -Name VMware.PowerCLI)) {Install-Module -Name VMware.PowerCLI -AllowClobber}
+
 # Add Module
 Import-Module VMware.DeployAutomation
+Set-PowerCLIConfiguration -Scope User -ParticipateInCEIP $false -ErrorAction SilentlyContinue
+Set-PowerCLIConfiguration -Scope User -InvalidCertificateAction Ignore -ErrorAction SilentlyContinue
 
 # Connect to vCenter Server
 Connect-viserver $VCenter -user $VCUser -password $VCPwd -WarningAction 0
 
-# SQL-01
+# DC-01
 
-$VMName = "SQL-01"
-$MAC = "00:50:56:1D:20:01"
+$VMName = "DC-01"
+$MAC = "00:50:56:00:50:01"
 $VRAM = 2048
 $VCPU = 2
 $VMDiskGB = 40
@@ -48,73 +94,83 @@ $VMDiskGB = 40
 $LogPS = "${env:SystemRoot}" + "\Temp\$VMName.log"
 Start-Transcript $LogPS
 
-New-VM -Name $VMName -VMHost $ESXi -numcpu $VCPU -MemoryMB $VRAM -DiskGB $VMDiskGB -DiskStorageFormat $VMDiskType -Datastore $VMDS -GuestId $VMGuestOS -NetworkName $NetName -CD
+New-VM -Name $VMName -VMHost $ESXi -numcpu $VCPU -MemoryMB $VRAM -DiskGB $VMDiskGB -DiskStorageFormat $VMDiskType -Datastore $VMDS -GuestId $VMGuestOS -NetworkName $NetName
 Get-VM $VMName | Get-NetworkAdapter | Set-NetworkAdapter -Type $NICType -MacAddress $MAC -StartConnected:$true -Confirm:$false
-Get-VM $VMName | Get-CDDrive | Set-CDDrive -ISOPath $ISO -StartConnected:$true -Confirm:$false
+
+$VM = Get-VM $VMName
+$spec = New-Object VMware.Vim.VirtualMachineConfigSpec
+$spec.Firmware = [VMware.Vim.GuestOsDescriptorFirmwareType]::efi
+$vm.ExtensionData.ReconfigVM($spec)
+Get-VM -Name $VMName | Set-SecureBoot -Enabled
+Start-Sleep -s 15
 Start-VM -VM $VMName -confirm:$false -RunAsync
 
-# VCS-01
+Start-Sleep -s 1500
 
-$VMName = "VCS-01"
-$MAC = "00:50:56:1D:20:04"
+# DC-02
+
+$VMName = "DC-02"
+$MAC = "00:50:56:00:50:02"
+$VRAM = 2048
+$VCPU = 2
+$VMDiskGB = 40
+
+$LogPS = "${env:SystemRoot}" + "\Temp\$VMName.log"
+Start-Transcript $LogPS
+
+New-VM -Name $VMName -VMHost $ESXi -numcpu $VCPU -MemoryMB $VRAM -DiskGB $VMDiskGB -DiskStorageFormat $VMDiskType -Datastore $VMDS -GuestId $VMGuestOS -NetworkName $NetName
+Get-VM $VMName | Get-NetworkAdapter | Set-NetworkAdapter -Type $NICType -MacAddress $MAC -StartConnected:$true -Confirm:$false
+
+$VM = Get-VM $VMName
+$spec = New-Object VMware.Vim.VirtualMachineConfigSpec
+$spec.Firmware = [VMware.Vim.GuestOsDescriptorFirmwareType]::efi
+$vm.ExtensionData.ReconfigVM($spec)
+Get-VM -Name $VMName | Set-SecureBoot -Enabled
+Start-Sleep -s 15
+Start-VM -VM $VMName -confirm:$false -RunAsync
+
+# RAS-WS19-GW
+
+$VMName = "RAS-WS19-GW"
+$MAC = "00:50:56:00:50:04"
+$VRAM = 2048
+$VCPU = 2
+$VMDiskGB = 40
+
+$LogPS = "${env:SystemRoot}" + "\Temp\$VMName.log"
+Start-Transcript $LogPS
+
+New-VM -Name $VMName -VMHost $ESXi -numcpu $VCPU -MemoryMB $VRAM -DiskGB $VMDiskGB -DiskStorageFormat $VMDiskType -Datastore $VMDS -GuestId $VMGuestOS -NetworkName $NetName
+Get-VM $VMName | Get-NetworkAdapter | Set-NetworkAdapter -Type $NICType -MacAddress $MAC -StartConnected:$true -Confirm:$false
+
+$VM = Get-VM $VMName
+$spec = New-Object VMware.Vim.VirtualMachineConfigSpec
+$spec.Firmware = [VMware.Vim.GuestOsDescriptorFirmwareType]::efi
+$vm.ExtensionData.ReconfigVM($spec)
+Get-VM -Name $VMName | Set-SecureBoot -Enabled
+Start-Sleep -s 15
+Start-VM -VM $VMName -confirm:$false -RunAsync
+
+# RAS-WS19-01
+
+$VMName = "RAS-WS19-01"
+$MAC = "00:50:56:00:50:05"
 $VRAM = 4096
-$VCPU = 4
-$VMDiskGB = 40
-
-$LogPS = "${env:SystemRoot}" + "\Temp\$VMName.log"
-Start-Transcript $LogPS
-
-New-VM -Name $VMName -VMHost $ESXi -numcpu $VCPU -MemoryMB $VRAM -DiskGB $VMDiskGB -DiskStorageFormat $VMDiskType -Datastore $VMDS -GuestId $VMGuestOS -NetworkName $NetName -CD
-Get-VM $VMName | Get-NetworkAdapter | Set-NetworkAdapter -Type $NICType -MacAddress $MAC -StartConnected:$true -Confirm:$false
-Get-VM $VMName | Get-CDDrive | Set-CDDrive -ISOPath $ISO -StartConnected:$true -Confirm:$false
-Start-VM -VM $VMName -confirm:$false -RunAsync
-
-# CLIENT-01
-
-$VMName = "CLIENT-01"
-$MAC = "00:50:56:1D:20:03"
-$VRAM = 2048
 $VCPU = 2
-$VMDiskGB = 40
+$VMDiskGB = 60
 
 $LogPS = "${env:SystemRoot}" + "\Temp\$VMName.log"
 Start-Transcript $LogPS
 
-New-VM -Name $VMName -VMHost $ESXi -numcpu $VCPU -MemoryMB $VRAM -DiskGB $VMDiskGB -DiskStorageFormat $VMDiskType -Datastore $VMDS -GuestId $VMGuestOS -NetworkName $NetName -CD
+New-VM -Name $VMName -VMHost $ESXi -numcpu $VCPU -MemoryMB $VRAM -DiskGB $VMDiskGB -DiskStorageFormat $VMDiskType -Datastore $VMDS -GuestId $VMGuestOS -NetworkName $NetName
 Get-VM $VMName | Get-NetworkAdapter | Set-NetworkAdapter -Type $NICType -MacAddress $MAC -StartConnected:$true -Confirm:$false
-Get-VM $VMName | Get-CDDrive | Set-CDDrive -ISOPath $ISO -StartConnected:$true -Confirm:$false
-Start-VM -VM $VMName -confirm:$false -RunAsync
 
-# WS16RDSH-01
-
-$VMName = "WS16RDSH-01"
-$MAC = "00:50:56:1D:20:02"
-$VRAM = 2048
-$VCPU = 2
-$VMDiskGB = 100
-
-$LogPS = "${env:SystemRoot}" + "\Temp\$VMName.log"
-Start-Transcript $LogPS
-
-New-VM -Name $VMName -VMHost $ESXi -numcpu $VCPU -MemoryMB $VRAM -DiskGB $VMDiskGB -DiskStorageFormat $VMDiskType -Datastore $VMDS -GuestId $VMGuestOS -NetworkName $NetName -CD
-Get-VM $VMName | Get-NetworkAdapter | Set-NetworkAdapter -Type $NICType -MacAddress $MAC -StartConnected:$true -Confirm:$false
-Get-VM $VMName | Get-CDDrive | Set-CDDrive -ISOPath $ISO -StartConnected:$true -Confirm:$false
-Start-VM -VM $VMName -confirm:$false -RunAsync
-
-# WS16RDSH-01
-
-$VMName = "WS16RDSH-02"
-$MAC = "00:50:56:1D:20:05"
-$VRAM = 2048
-$VCPU = 2
-$VMDiskGB = 100
-
-$LogPS = "${env:SystemRoot}" + "\Temp\$VMName.log"
-Start-Transcript $LogPS
-
-New-VM -Name $VMName -VMHost $ESXi -numcpu $VCPU -MemoryMB $VRAM -DiskGB $VMDiskGB -DiskStorageFormat $VMDiskType -Datastore $VMDS -GuestId $VMGuestOS -NetworkName $NetName -CD
-Get-VM $VMName | Get-NetworkAdapter | Set-NetworkAdapter -Type $NICType -MacAddress $MAC -StartConnected:$true -Confirm:$false
-Get-VM $VMName | Get-CDDrive | Set-CDDrive -ISOPath $ISO -StartConnected:$true -Confirm:$false
+$VM = Get-VM $VMName
+$spec = New-Object VMware.Vim.VirtualMachineConfigSpec
+$spec.Firmware = [VMware.Vim.GuestOsDescriptorFirmwareType]::efi
+$vm.ExtensionData.ReconfigVM($spec)
+Get-VM -Name $VMName | Set-SecureBoot -Enabled
+Start-Sleep -s 15
 Start-VM -VM $VMName -confirm:$false -RunAsync
 
 Write-Verbose "Stop logging" -Verbose
